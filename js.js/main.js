03cfd935d392284f5d207d457b03c02012fc2828
BORETS2002 <BORETS2002@example.com>
2022-08-01 23:33:35 +0300

    pags end

--- js.js/main.js
+++ js.js/main.js
@@ -6,3 +6,16 @@ elSiteheaderToggleBtn.addEventListener("click", function () {
   elHeader.classList.toggle("site-header__open");
   elsiteNav.classList.toggle("site-hader__open");
 });
+
+var elModalOpenButton = document.querySelector(".js-modal-open-button");
+var elModal = document.querySelector(".modal");
+
+elModalOpenButton.addEventListener("click", function () {
+  elModal.classList.add("modal-show");
+});
+
+var elModalCloseButton = document.querySelector(".js-close-modal");
+
+elModalCloseButton.addEventListener("click", function () {
+  elModal.classList.remove("modal-show");
+});
